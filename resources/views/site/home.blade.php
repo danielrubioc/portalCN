f5542468f8474805a292218fcba8542c1c1ff91b
DanielRubio2401 <daniel@mingadigital.com>
2018-01-09 21:53:04 -0300

    se actualiza home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -24,7 +24,7 @@
 			<div class="text-description align-left-section">
 				<div class="text-description-section text-second-gral">
 					<h1>Nosotros</h1>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+					<p>Buscamos brindar a todos los vecinos y vecinas variados talleres Deportivos y de Actividad Física para que puedan desarrollar habilidades de destrezas corporales, generar una vida más saludable y activa, que permita un desarrollo de mejora de su calidad de vida.</p>
 					
 				</div>
 			</div>
@@ -40,7 +40,7 @@
 			<div class="text-description align-left-section">
 				<div class="text-description-section text-third-gral">
 					<h1>Disciplina</h1>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+					<p>Te invitamos a conocer las variadas disciplinas, talleres y cursos que tenemos a disposición de todos nuestros vecinos y vecinas. ¡Inscríbete y mejora tu calidad de vida!</p>
 					
 				</div>
 			</div>
@@ -57,7 +57,7 @@
 			<div class="text-description align-left-section">
 				<div class="text-description-section text-four-gral">
 					<h1>Eventos</h1>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+					<p>Revisa las actividades especiales que la Corporación del Deporte organiza para todos sus vecinos. ¡Entérate e inscríbete, participa y comparte!</p>
 					
 				</div>
 			</div>
@@ -73,7 +73,7 @@
 			<div class="text-description align-left-section">
 				<div class="text-description-section text-five-gral">
 					<h1>Tercer tiempo</h1>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+					<p>Visita la sección Tercer tiempo y aprende más sobre todos los aspectos de la Corporación del Deporte, desde notas y artículos, hasta historias y experiencias de vida de nuestros vecinos y vecinas. ¡Entra, aprende y comparte!</p>
 					
 				</div>
 			</div>
@@ -89,12 +89,12 @@
 			<div class="text-description align-left-section">
 				<div class="text-description-section text-six-gral">
 					<h1>Contacto</h1>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+					<p>Mantengamos el contacto.</p>
 					
 				</div>
 			</div>
 			<div class="content-btn-full">
-				<a href="" class="btn-first btn-gral">Aprende más</a>
+				<a href="{{ url('/contacto') }}" class="btn-first btn-gral">Aprende más</a>
 			</div>
 		</div>
 	</section>
