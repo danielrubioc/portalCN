8e618fc1ec85679d09b0b40665d335bf08b5f333
pabloossandon88 <pabloossandon88@gmail.com>
2018-01-23 14:52:58 -0300

    email return

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -3,7 +3,7 @@
 	<title></title>
 </head>
 <body>
-	<p> <strong>Hola {{ $name }} </strong>  </p>
+	<p> <strong>Hola {{ $nombre }} </strong>  </p>
 	<p> Te haz registrado en la deportes cerro navia </p>
 	<p> Ingresa este codigo : <strong> {{ $codigo }} </strong> y tu usuario quedara validado.</p>
 	<p> ... </p>
