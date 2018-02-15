acef3c0bf7547b7071c719024babbc99547730dd
DanielRubio2401 <daniel@mingadigital.com>
2018-02-15 19:38:56 -0300

    migracion de banner

---
+++ database/migrations/2018_02_15_214549_add_column_to_banner_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToBannerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('banners', function (Blueprint $table) {
+            //
+            $table->string('url');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('banners', function (Blueprint $table) {
+            //
+        });
+    }
+}
