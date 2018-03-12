49d263f16c2cf92830b7805f635d071c5043768e
DanielRubio2401 <daniel@mingadigital.com>
2018-03-12 20:52:37 -0300

    email validacion

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -5,7 +5,7 @@
 <body>
 	<p>Hola <strong>{{ $nombre }}</strong></p>
 	<p> Te haz registrado en Corporación Deportes Cerro Navia </p>
-	<p> Ingresa este codigo : <strong> {{ $codigo }} </strong> y tu usuario quedara validado.</p>
+	<p> Ingresa este código : <strong> {{ $codigo }} </strong> y tu usuario quedará validado.</p>
 	<p> ... </p>
 </body>
 </html>
