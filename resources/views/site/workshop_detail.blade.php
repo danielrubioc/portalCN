3de1d57cebda8067cabfe7e7dc449f2a575e17cf
DanielRubio2401 <daniel@mingadigital.com>
2018-03-18 13:26:45 -0300

    actualizacion

--- resources/views/site/workshop_detail.blade.php
+++ resources/views/site/workshop_detail.blade.php
@@ -442,7 +442,150 @@
 								</div>
 							</div>
 
-							
+
+							<div class="col-md-12 form-group{{ $errors->has('birth_date') ? ' has-error' : '' }}">
+								<div class="input-effect">
+									<input id="birth_date" type="date" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="birth_date" value="{{ old('birth_date') }}" required autofocus>
+									<label for="birth_date" style="top: -16px;">Fecha de nacimiento</label>
+									<span class="focus-border"></span>
+									@if ($errors->has('birth_date'))
+										<span class="help-block">
+											<strong>{{ $errors->first('birth_date') }}</strong>
+										</span>
+									@endif
+								</div>
+							</div>
+
+							<div class="col-md-12 form-group{{ $errors->has('age') ? ' has-error' : '' }}">
+								<div class="input-effect">
+									<input id="age" type="text" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="age" value="{{ old('age') }}" required readonly>
+									<label for="age"  style="top: -16px;">Edad</label>
+									<span class="focus-border"></span>
+									@if ($errors->has('age'))
+										<span class="help-block">
+											<strong>{{ $errors->first('age') }}</strong>
+										</span>
+									@endif
+								</div>
+							</div>
+
+
+							<div class="form-group{{ $errors->has('age') ? ' has-error' : '' }} col-md-12">    
+                                <label for="email" style="color: #aaa;">Tienes problemas de salud?</label>
+	                                <div class="radio" style="margin-top: -10px;">
+	                                  <label class="checkbox-inline"><input type="radio" name="health" class="closeheal" id="health" value="0" checked>No</label>
+	                                  <label class="checkbox-inline"><input type="radio" class="openheal" name="health" id="health" value="1">Si</label>
+	                                </div>
+	                            <div class="shownDiv" style="display:none;">
+	                                <div class="form-group{{ $errors->has('health_problem') ? ' has-error' : '' }}">
+	                                    <div class="col-md-12 effect-reg">
+	                                       	<label for="health_problem" class="control-label" style="color:#aaa; text-align: left;">Especifique</label>
+	                                        <textarea id="health_problem" type="text" class="form-control not-empty {{ !empty($errors->first()) ? ' empty' : '' }} no-bor" name="health_problem" value="{{ old('health_problem') }}"></textarea>
+
+	                                        @if ($errors->has('health_problem'))
+	                                            <span class="help-block">
+	                                                <strong>{{ $errors->first('health_problem') }}</strong>
+	                                            </span>
+	                                        @endif
+	                                    </div>
+	                                </div>
+
+	                            </div>
+	                        </div> 
+
+	                        @if(!old('age'))
+	                            <span class="titular-info" style="display:none;">
+	                        @else
+	                            @if(old('age') >= 18)
+	                                <span class="titular-info" style="display:none;">
+	                                @else
+	                                <span class="titular-info" style="display:block;">
+	                            @endif
+	                        @endif
+                                
+
+								<div class="col-md-12 form-group{{ $errors->has('school') ? ' has-error' : '' }}">
+									<div class="input-effect">
+										<input id="school" type="text" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="school" value="{{ old('school') }}" required autofocus>
+										<label for="school" >Nombre del colegio al que asiste?</label>
+										<span class="focus-border"></span>
+										@if ($errors->has('school'))
+											<span class="help-block">
+												<strong>{{ $errors->first('school') }}</strong>
+											</span>
+										@endif
+									</div>
+								</div>
+
+								<div class="col-md-12 form-group{{ $errors->has('headline_full_name') ? ' has-error' : '' }}">
+									<div class="input-effect">
+										<input id="headline_full_name" type="text" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="headline_full_name" value="{{ old('headline_full_name') }}" required autofocus>
+										<label for="headline_full_name" >Nombre completo (apoderado)</label>
+										<span class="focus-border"></span>
+										@if ($errors->has('headline_full_name'))
+											<span class="help-block">
+												<strong>{{ $errors->first('headline_full_name') }}</strong>
+											</span>
+										@endif
+									</div>
+								</div>
+
+
+								<div class="col-md-12 form-group{{ $errors->has('headline_phone') ? ' has-error' : '' }}">
+									<div class="input-effect">
+										<input id="headline_phone" type="text" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="headline_phone" value="{{ old('headline_phone') }}" required autofocus>
+										<label for="headline_phone" >Teléfono de contacto (apoderado)</label>
+										<span class="focus-border"></span>
+										@if ($errors->has('headline_phone'))
+											<span class="help-block">
+												<strong>{{ $errors->first('headline_phone') }}</strong>
+											</span>
+										@endif
+									</div>
+								</div>
+
+								<div class="col-md-12 form-group{{ $errors->has('headline_email') ? ' has-error' : '' }}">
+									<div class="input-effect">
+										<input id="headline_email" type="email" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="headline_email" value="{{ old('headline_email') }}" required autofocus>
+										<label for="headline_email" >E-Mail (apoderado)</label>
+										<span class="focus-border"></span>
+										@if ($errors->has('headline_email'))
+											<span class="help-block">
+												<strong>{{ $errors->first('headline_email') }}</strong>
+											</span>
+										@endif
+									</div>
+								</div>
+                               
+								<div class="col-md-12 form-group{{ $errors->has('headline_rut') ? ' has-error' : '' }}">
+									<div class="input-effect">
+										<input id="headline_rut" type="text" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="headline_rut" value="{{ old('headline_rut') }}" title="ej. 12345678-5" pattern="[0-9]{7,8}-[0-9Kk]{1}" required autofocus>
+										<label for="headline_rut" >Rut (apoderado)</label>
+										<span class="focus-border"></span>
+										@if ($errors->has('headline_rut'))
+											<span class="help-block">
+												<strong>{{ $errors->first('headline_rut') }}</strong>
+											</span>
+										@endif
+									</div>
+								</div>
+                               
+
+                        	</span>
+
+                        	<div class="col-md-12 form-group">
+							  	<label for="sel1" style="color: #aaa;">Como te enteraste del taller?</label>
+							  	<select class="form-control no-bor" name="commentary" id="commentary" value="{{ old('commentary') }}" required>
+								  	<option value="">-Selecciona una opcion</option>
+								    <option value="Facebook Deportes">Facebook Deportes</option>
+								    <option value="Facebook Municipalidad">Facebook Municipalidad</option>
+								    <option value="Volante o Afiche">Volante o Afiche</option>
+								    <option value="Página Web Municipalidad">Página Web Municipalidad</option>
+								    <option value="Boca a boca">Boca a boca</option>
+								    <option value="Diario Comunal "Barrancas"">Diario Comunal "Barrancas"</option>
+							  	</select>
+							</div>
+
 							<div class="col-md-12 form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 								<div class="input-effect">
 									<input id="password" type="password" class="form-control effect-placeholder {{ !empty($errors->first()) ? ' empty' : '' }}" name="password" value="{{ old('password') }}" required autofocus>
