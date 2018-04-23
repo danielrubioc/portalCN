7829a9d0302e3cb2b5da04fb49e6fee538f1b317
Daniel Rubio <daniel@mingadigital.com>
2018-04-23 16:05:30 -0300

    share facebook

--- resources/views/site/post_detail.blade.php
+++ resources/views/site/post_detail.blade.php
@@ -24,7 +24,7 @@
 								  	onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
 								<span class="fa fa-twitter"></span> </a>
 							</li>
-					  		<li class="hidden-xs hidden-sm fbc-face"><div class="fb-share-button" data-href="{{URL::current()}}" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fcerronaviadeporte%2F&amp;src=sdkpreparse"><span class="fa fa-facebook"></span> </a></a></div></li>
+					  		<li class="hidden-xs hidden-sm fbc-face"><div class="fb-share-button" data-href="https://www.facebook.com/cerronaviadeporte/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}"><span class="fa fa-facebook"></span> </a></a></div></li>
 
 					   	</ul>
 						<h4 class="subtitle">{{ $post->subtitle }}</h4>
@@ -84,7 +84,7 @@
 							<div class="social-icons-detail">
 								<ul>
 								
-									<li id="btn-1-left"><div class="fb-share-button" data-href="{{URL::current()}}" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fcerronaviadeporte%2F&amp;src=sdkpreparse"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
+									<li id="btn-1-left"><div class="fb-share-button" data-href="https://www.facebook.com/cerronaviadeporte/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fcerronaviadeporte%2F&amp;src=sdkpreparse"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
 									</li>
 									<li id="btn-2-right"><a href="whatsapp://send?text={{ url()->current() }}" data-action="share/whatsapp/share"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
 								</ul>
@@ -150,8 +150,9 @@
 		<hr class="separator-comparte">
 		<div class="social-icons">
 			<ul>
+
 				<!--<li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>-->
-				<li><div class="fb-share-button" data-href="{{URL::current()}}" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fcerronaviadeporte%2F&amp;src=sdkpreparse"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
+				<li><a class="fb-xfbml-parse-ignore" href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}" target="_blank" ><i class="fa fa-facebook" aria-hidden="true"></i></a>
 				</li>
 				<li><a href="whatsapp://send?text={{ url()->current() }}" data-action="share/whatsapp/share"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
 			</ul>
@@ -162,116 +163,7 @@
 	</span>
 @section('slider-owl')
 
-<script type="text/javascript">
 
-
-
-var scrollTop = $('html body').scrollTop();
-
-
-console.log(scrollTop +'<= '+($('.info-content-related-news').offset().top - 300));
-
-$(window).scroll(function(){
-
-if(scrollTop <= ($('.info-content-related-news').offset().top - 300)){
-	console.log('ss'+scrollTop);
-
-}
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//on load
-var scrollTop = $('html body').scrollTop();
-
-
-if(scrollTop >= ($('.related-bx').offset().top)){
-	$("#post-related-0").addClass( "in" );
-	$("#post-related-1").addClass( "in" );
-	$("#post-related-2").addClass( "in" );
-
-}
-if(scrollTop >= 200){
-	$( "#box1" ).css('transition', '0.8s all');
-	$( "#box1" ).css('top', '100px');
-	$("#box1").addClass( "in" );
-    $("#btn-1-left").addClass( "in" );
-    $("#btn-2-right").addClass( "in" ); 	
-}
-
-var countSum =  0 ; 
-var countRes =  0 ; 
-var lastScrollTop = 0;
-
-
-$(window).scroll(function(){
-	var scrollTop = $('html body').scrollTop();
-	var st = $(this).scrollTop();
-	if(scrollTop >= 250){
-
-	  	if (st > lastScrollTop){
-       	// downscroll code
-		    $("#box1").addClass( "in" );
-		    
-		    $("#btn-1-left").addClass( "in" );
-		    $("#btn-2-right").addClass( "in" ); 
-		    $("#btn-2-right").addClass( "in" ); 
-		    $( "#box1" ).css('position', 'fixed');
-		    $( "#box1" ).css('transition', '0s all');
-		    $( "#box1" ).css('top', '40%');
-		    $( "#box1" ).css('width', '50%');
-		   
-	   	} 
-	   	if (scrollTop >= $('.related-bx').offset().top - 700 && countSum == 0) {
-	   		console.log($('#box1').offset().top);
-	   		var top = $('#box1').offset().top;
-	   		$( "#box1" ).css('position', 'absolute');
-	   		$( "#box1" ).css('top', top+'px!important');
-			$( "#box1" ).css('width', '100%');
-			countSum++;
-	   	};
-
-	} else{
-		$( "#box1" ).css('position', 'absolute');
-
-		$( "#box1" ).css('width', '100%');
-	}
-
-	if(scrollTop >= ($('.related-bx').offset().top - 500)){
-		$("#post-related-0").addClass( "in" );
-		$("#post-related-1").addClass( "in" );
-		$("#post-related-2").addClass( "in" );
-
-	}	
-	lastScrollTop = st;
-
-})
-
-
-</script>
 
 @stop
 
